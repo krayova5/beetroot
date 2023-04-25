7e20cb7fcafbae72e0878cc2a2f50dabc80e6c7c
kolomiiets <krayova5@gmail.com>
2023-04-25 12:54:04 +0300

    Final edit

---
+++ lesson29-js-basic/index.js
@@ -0,0 +1,19 @@
+const userNameOne = 'John';
+const userNameTwo = 'Frank';
+const userNameThree = 'Justin';
+const userNameForth = 'Gregory';
+
+//const userNameOne = 'Jane';
+//const userNameTwo = 'Rachel';
+/*const userNameThree = 'Mary';
+const userNameForth = 'Lucy';
+const userNameFive = 'Nancy'*/
+
+
+console.log(userNameTwo);
+
+const NumberOne = 5;
+const NumberTwo = 6;
+const NumberThree = 6;
+
+console.log( "Середнє арифметичне:"(NumberOne + NumberTwo + NumberThree)/3 );
