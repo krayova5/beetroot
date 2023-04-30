f77aa893bf89ce9a8b7280aa5999f26f6f6e4ff7
kolomiiets <krayova5@gmail.com>
2023-04-30 15:12:19 +0300

    Correct mistakes

--- lesson30-data-types/index.js
+++ lesson30-data-types/index.js
@@ -1,28 +1,33 @@
 //Task 1
-let a = 0.1;
-let b = 0.2;
-console.log((a + b).toFixed(1));
+const firstNum = 0.1;
+const secondNum = 0.2;
+const sum = (firstNum + secondNum).toFixed(1);
+console.log(sum);
 
 //Task 2
-let c = "1";
-let d = 2;
-console.log( +c + d );
+const string = "1";
+const number = 2;
+const sum1 = +string + number;
+console.log(sum1);
 
 //Task 3
 
-let volume = 820;
-let volumeFlashCard = prompt("If the flash drive size is " )
-alert (`You can save ${Math.floor(volumeFlashCard*1024/volume)} files`);
+const volume = 820;
+const volumeFlashCard = prompt("If the flash drive size is " );
+const amountFiles = Math.floor(volumeFlashCard * 1024 / volume);
+alert(`You can save ${amountFiles} files`);
 
 //Task 4
-let moneySum = prompt ("How much money you have ?");
-let sweetsPrice = prompt ("What is the price of chocolates ?");
-alert(`You can have ${(Math.floor(moneySum / sweetsPrice))} chocolates 
-and ${(moneySum-(Math.floor(moneySum/ sweetsPrice)) * sweetsPrice)} USD`);
+const moneySum = prompt ("How much money you have ?");
+const sweetsPrice = prompt ("What is the price of chocolates ?");
+const amountChoco = Math.floor(moneySum / sweetsPrice);
+const rest = moneySum - Math.floor(moneySum / sweetsPrice) * sweetsPrice;
+alert(`You can have ${amountChoco} chocolates 
+and ${rest} USD`);
 
 //Task 5
 
 const userNumber = prompt("Would do you say three digit number ? ");
-let reverseNumber = userNumber.split("");
+const reverseNumber = userNumber.split("");
 alert(`You reverse number is ${reverseNumber.reverse().join('')}`);
 
--- lesson31-loops-cond/index.js
+++ lesson31-loops-cond/index.js
@@ -1,5 +1,70 @@
+
+/*Task 1 Запитай у користувача його вік і визначи, ким він є: дитиною (0-11), підлітком (12-17), дорослим (18_59) або пенсіонером (60 ...), передбач можливість введення невірних даних.
+
+ const userAge = prompt ("How old are you ?");
+
+ if (userAge <= 11) {
+  console.log("You are a child.");
+
+ } else if( userAge > 11 ,  userAge <= 17) {
+   console.log("You are a teenager.");
+  
+ } else if (userAge > 17, userAge <= 59) {
+   console.log("You are an adult.");
+ } else 
+   console.log("You are a retiree.");*/
+
+
+/*Task 2 Запитай у користувача число від 0 до 9 і виведи йому спецсимвол, який розташований на цій клавіші (1 !, 2 @, 3 # і т. д).*/
+
+/*const userNumber = prompt("What is you number from 0 to 9?");
+switch (userNumber) {
+  case '0':
+    console.log("symbol = )");
+    break;
+  case '1':
+    console.log("symbol = !");
+    break; break;
+  case '2':
+    console.log("symbol = @");
+    break;
+  case '3':
+    console.log("symbol = #");
+    break;
+  case '4':
+    console.log("symbol = $");
+    break;
+  case '5':
+    console.log("symbol = %");
+    break;
+  case '6':
+    console.log("symbol = ^");
+    break;
+  case '7':
+    console.log("symbol = &");
+    break;
+  case '8':
+    console.log("symbol = *");
+    break;
+  case'9':
+    console.log("symbol = (");
+    break;
+  default:
+    console.log("Invalid value");
+    break;
+}
+
+/*Task  Підрахуй суму всіх чисел в заданому користувачем діапазоні.*/
+
+ const userRange = prompt("Set digital range ");
+let sum = item[0];
+for (const item of userRange) {
+  sum = sum + item
+}
+console.log(sum);
+
 //Task 1
-let a = 0.1;
+/*let a = 0.1;
 let b = 0.2;
 console.log((a + b).toFixed(1));
 
