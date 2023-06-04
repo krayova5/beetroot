1d6eb601944f4fab33a5a4b50a5fbbe9cb981e46
kolomiiets <krayova5@gmail.com>
2023-06-04 18:49:45 +0300

    Create tasks

---
+++ lesson39-webstorage/index.js
@@ -0,0 +1,23 @@
+window.myWidgetParam ? window.myWidgetParam : (window.myWidgetParam = []);
+window.myWidgetParam.push({
+  id: 2,
+  cityid: "710791",
+  appid: "3fe17f75164d18b5fabf748e2c2a3665",
+  units: "metric",
+  containerid: "openweathermap-widget-2",
+});
+(function () {
+  var script = document.createElement("script");
+  script.async = true;
+  script.charset = "utf-8";
+  script.src =
+    "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(script, s);
+})();
+
+const API_KEY = "3fe17f75164d18b5fabf748e2c2a3665";
+https: //api.openweathermap.org / data / 3.0 / onecall ? lat = { lat } & lon={ lon }& exclude={ part }& appid={ 3fe17f75164d18b5fabf748e2c2a3665}
+gettigWeather = async () => {
+  const api
+}
