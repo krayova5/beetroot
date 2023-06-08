47ab4a5d2795ba8ca7ad4c5e686b68b1365ad1fc
kolomiiets <krayova5@gmail.com>
2023-06-08 16:21:04 +0300

    New HW 43

--- lesson43-inheritance/index.js
+++ lesson43-inheritance/index.js
@@ -23,27 +23,86 @@ class User {
   getName() {
     return this.name;
   }
+
   getRole() {
     return this.role;
-  };
-  login() {}
-  logout() {}
+  }
 
-  changeName() {}
-  changePassword() {}
+  login() {
+   console.log('User logged in.');
+  }
+
+  logout() {
+    console.log('User logged out.');
+  }
+
+  changeName(newName) {
+    this.name = newName;
+  }
+
+  changePassword(newPassword) {
+    console.log('Password changed.');
+  }
 }
 
 class Admin extends User {
-  addUser() {
-    return super.getName();
+  constructor(name) {
+    super(name, 'admin');
+    this.users = [];
   }
 
-  removeUser() {}
-  
-
-  changeUserRole() {
-    return super.getRole("admin");
+  addUser(user) {
+    if (user instanceof User) {
+      this.users.push(user);
+      console.log('User added:', user.getName());
+    } else {
+      alert('Invalid user object.');
+    }
   }
-  getAllUsers() {}
-  removeAllUsers() {}
+
+  removeUser(user) {
+    const index = this.users.indexOf(user);
+    if (index !== -1) {
+      const removedUser = this.users.splice(index, 1)[0];
+      console.log('User removed:', removedUser.getName());
+    } else {
+      console.log('User not found.');
+    }
+  }
+
+  changeUserRole(user, newRole) {
+    if (user instanceof User) {
+      user.role = newRole;
+      console.log('User role changed:', user.getName());
+    } else {
+      alert('Invalid user object.');
+    }
+  }
+
+  getAllUsers() {
+    return this.users;
+  }
+
+  removeAllUsers() {
+    this.users = [];
+    console.log('All users removed.');
+  }
 }
+
+
+const user1 = new User('Petro', 'admin');
+//console.log(user1.getName());  
+//console.log(user1.getRole()); 
+user1.login();                 
+
+const admin1 = new Admin('Admin');
+const user2 = new User('John', 'user');
+admin1.addUser(user1);         
+admin1.addUser(user2);         
+//console.log(admin1.getAllUsers());  
+admin1.removeUser(user1);      
+//console.log(admin1.getAllUsers()); 
+admin1.changeUserRole(user2, 'admin');
+//console.log(user2.getRole()); 
+admin1.removeAllUsers();      
+//console.log(admin1.getAllUsers());  
